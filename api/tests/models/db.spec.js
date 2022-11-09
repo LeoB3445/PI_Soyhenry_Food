a7588b1d72bc60a65b86770a7651803446520a64
Leonardo Bonetto <leonardo1bonetto@protonmail.com>
2022-11-09 18:35:44 -0300

    finished database tests

---
+++ api/tests/models/db.spec.js
@@ -0,0 +1,27 @@
+const { expect } = require('chai');
+const {Recipe, Diet, conn} = require('../../src/db');
+
+const dummyRecipe = {
+    name: 'Pochoclo',
+    summary: 'es pochoclo',
+    health_score: 5,
+    step_by_step:'cocina el pochoclo'
+}
+const dummyDiet ={
+    name:'seafood diet. I sea-food, i eat it'
+}
+
+describe('associations', ()=>{
+    before(() => conn.authenticate()
+    .catch((err) => {
+      console.error('Test enviromnet setup error:', err);
+    }));
+
+    it('should allow the setting and getting of associated elements', ()=>{
+        Promise.all([Recipe.create(dummyRecipe), Diet.create(dummyDiet)])
+        .then(([recipe,diet]) =>
+            recipe.setDiets([diet])
+            .then(()=> expect(recipe.getDiets).to.equal([diet]))
+        )
+    })
+})
--- api/tests/models/recipe.spec.js
+++ api/tests/models/recipe.spec.js
@@ -1,6 +1,13 @@
 const { Recipe, conn } = require('../../src/db.js');
 const { expect } = require('chai');
 
+const dummy = {
+  name: 'Pochoclo',
+  summary: 'es pochoclo',
+  health_score: 5,
+  step_by_step:'cocina el pochoclo'
+}
+
 describe('Recipe model', () => {
   before(() => conn.authenticate()
     .catch((err) => {
@@ -8,15 +15,48 @@ describe('Recipe model', () => {
     }));
   describe('Validators', () => {
     beforeEach(() => Recipe.sync({ force: true }));
-    describe('name', () => {
-      it('should throw an error if name is null', (done) => {
-        Recipe.create({})
-          .then(() => done(new Error('It requires a valid name')))
-          .catch(() => done());
-      });
-      it('should work when its a valid name', () => {
-        Recipe.create({ name: 'Milanesa a la napolitana' });
-      });
+    
+  
+  it('should work when all parameters are valid', () => {
+    Recipe.create(dummy);
+  });
+  describe('name', ()=>{
+    it('should throw an error if name is null', (done) => {
+      Recipe.create({...dummy, name:null})
+        .then(() => done(new Error('It requires a valid name')))
+        .catch(() => done());
+    });
+    it('should not accept a non- string-coercible', (done)=>{
+      Recipe.create({...dummy, name:[1,2]})
+        .then((res) => done(new Error('It should not have accepted non string')))
+        .catch(() => done());
     });
   });
+  describe('id', ()=>{
+    it('should auto-increment', ()=>{
+      Recipe.create({dummy})
+      .then(dummy1=>{
+        Recipe.create({dummy})
+        .then(dummy2 =>expect(dummy2.id).to.equal(dummy1.id+1))
+      })
+    });
+  });
+  describe('health_score', ()=>{
+    it('should not accept a value under 0', (done)=>{
+      Recipe.create({...dummy, health_score:-1})
+        .then(() => done(new Error('It should require a value over 0')))
+        .catch(() => done());
+    });
+    it('should not accept a value over 10', (done)=>{
+      Recipe.create({...dummy, health_score:11})
+        .then(() => done(new Error('It should require a value under 10')))
+        .catch(() => done());
+    });
+  })
+  });
+  describe('step_by_step', ()=>{
+    it('should not break even if value is null', ()=>{
+      Recipe.create({...dummy, step_by_step:null})
+    })
+  })
 });
