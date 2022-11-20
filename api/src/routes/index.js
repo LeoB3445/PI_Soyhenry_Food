b9cc9cc44030b113bc6f35854208fc1e6129c3fd
Leonardo Bonetto <leonardo1bonetto@protonmail.com>
2022-11-20 14:53:06 -0300

    added routes to index

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,16 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
+const {recipes} = require('./recipes');
+const {diets} = require('diets');
 
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+router.use('/recipes', recipes);
+
 
 
 module.exports = router;
